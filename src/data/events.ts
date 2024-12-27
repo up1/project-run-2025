3479c9b717d72b66e47d211be180aa0e8189a348
up1 <somkiat.p@gmail.com>
2024-12-27 16:58:30 +0700

    Add events

--- src/data/events.ts
+++ src/data/events.ts
@@ -3,32 +3,22 @@ import { Event } from '../types/Event';
 export const events: Event[] = [
   {
     id: '1',
-    name: 'Thailand Ultra Trail Festival',
-    date: '2024-03-15',
-    location: 'Chiang Mai',
-    distance: '100km',
-    description: 'Experience the beautiful trails of Northern Thailand in this challenging ultra marathon.',
-    imageUrl: 'https://images.unsplash.com/photo-1452626038306-9aae5e071dd3',
-    registrationUrl: '#'
+    name: 'Khao Eto Camp Trail',
+    date: '2025-01-4',
+    location: 'ปราจีนบุรี',
+    distance: '25 km',
+    description: 'เขาอีโต้ แคมป์ เทรล ครั้งที่ 5 ป่านันทนาการน้ำตกเขาอีโต้ ป่าในเมืองเดิม อ.เมืองปราจีนบุรี จ.ปราจีนบุรี',
+    imageUrl: 'https://scontent.fbkk2-7.fna.fbcdn.net/v/t39.30808-6/449359292_974084904504798_3620928220511284313_n.jpg?stp=dst-jpg_p720x720_tt6&_nc_cat=111&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeHyHjg7HjhlDEAvddAJSTypuazqybz8IWG5rOrJvPwhYfhEwxid8J4i2EqAt2FktsI&_nc_ohc=L0O91wXGM0QQ7kNvgHlHP_5&_nc_oc=AdiMLadmaOfSOemzc4_DpIbMEux3IJyy2a9Z8cXVZH7Y0xWJt6N9nFa3S-DLeMjbNKnZ04m3celkSwKzUBLPnhYg&_nc_zt=23&_nc_ht=scontent.fbkk2-7.fna&_nc_gid=ASTk7ng4BjvZAD2QE0_Qu1-&oh=00_AYAbXJQ1JDzy67RGO4Pf-u-XQNuGGw3ZjXmJdc0Gs6Kx_w&oe=67744B0A',
+    registrationUrl: 'https://www.facebook.com/khaoetocamptrail'
   },
   {
     id: '2',
-    name: 'Khao Yai Ultra Trail',
+    name: 'Thailand Earth Trail Series @Mor Hin Khao, Chaiyaphum',
     date: '2024-03-22',
-    location: 'Khao Yai National Park',
-    distance: '80km',
-    description: 'Run through one of Thailand\'s most beautiful national parks.',
-    imageUrl: 'https://images.unsplash.com/photo-1483721310020-03333e577078',
-    registrationUrl: '#'
-  },
-  {
-    id: '3',
-    name: 'Phuket Ultra Trail',
-    date: '2024-04-05',
-    location: 'Phuket',
-    distance: '120km',
-    description: 'Coastal ultra marathon with breathtaking ocean views.',
-    imageUrl: 'https://images.unsplash.com/photo-1506784693919-ef06d93c28d2',
-    registrationUrl: '#'
+    location: 'ชัยภูมิ',
+    distance: '50 km',
+    description: 'วิ่งท้าลมหนาว โอบกอดเขา...ที่ชัยภูมิ 𝐓𝐄𝐓𝐒-𝟐𝟎𝟐𝟒 @𝐌𝐎𝐑 𝐇𝐈𝐍 𝐊𝐇𝐀𝐎🪨🌳',
+    imageUrl: 'https://scontent.fbkk2-7.fna.fbcdn.net/v/t39.30808-6/471584440_498483956584934_7896082082023687790_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeHbBiis0UDloKISPCdgjhF0uteCf7w-pEq614J_vD6kSkMdwcRxaJN0NoLGlc6Oswg&_nc_ohc=iB_T7QrCTJEQ7kNvgH-5vks&_nc_oc=AdhylCGF54fufju-6bCVpwIcxE6G1ZD7u9Ej73_9H41ohqwQE5WnyzhkndjJcGsrkjL_AsplI4qjGPJw9jdiF2W7&_nc_zt=23&_nc_ht=scontent.fbkk2-7.fna&_nc_gid=AMN8MN87h_fsqKCCjocSd4N&oh=00_AYDXUSjCZSowLdMAiCfRrdIfcZsoXu8UkVBAaw-4PHxL-A&oe=67745E8D',
+    registrationUrl: 'https://www.facebook.com/WarrixThailandEarthTrail'
   }
 ];
