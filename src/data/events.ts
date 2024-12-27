f6412cc4ee869bc11afeac36d7e55bf8c9c34f35
up1 <somkiat.p@gmail.com>
2024-12-27 17:04:35 +0700

    Add runs

--- src/data/events.ts
+++ src/data/events.ts
@@ -20,5 +20,25 @@ export const events: Event[] = [
     description: 'วิ่งท้าลมหนาว โอบกอดเขา...ที่ชัยภูมิ 𝐓𝐄𝐓𝐒-𝟐𝟎𝟐𝟒 @𝐌𝐎𝐑 𝐇𝐈𝐍 𝐊𝐇𝐀𝐎🪨🌳',
     imageUrl: 'https://scontent.fbkk2-7.fna.fbcdn.net/v/t39.30808-6/471584440_498483956584934_7896082082023687790_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeHbBiis0UDloKISPCdgjhF0uteCf7w-pEq614J_vD6kSkMdwcRxaJN0NoLGlc6Oswg&_nc_ohc=iB_T7QrCTJEQ7kNvgH-5vks&_nc_oc=AdhylCGF54fufju-6bCVpwIcxE6G1ZD7u9Ej73_9H41ohqwQE5WnyzhkndjJcGsrkjL_AsplI4qjGPJw9jdiF2W7&_nc_zt=23&_nc_ht=scontent.fbkk2-7.fna&_nc_gid=AMN8MN87h_fsqKCCjocSd4N&oh=00_AYDXUSjCZSowLdMAiCfRrdIfcZsoXu8UkVBAaw-4PHxL-A&oe=67745E8D',
     registrationUrl: 'https://www.facebook.com/WarrixThailandEarthTrail'
+  },
+  {
+    id: '3',
+    name: 'Pha Tang Trail',
+    date: '2025-01-11',
+    location: 'เชียงราย',
+    distance: '50 km',
+    description: 'PHA TANG TRAIL 2025',
+    imageUrl: 'https://scontent.fbkk2-7.fna.fbcdn.net/v/t39.30808-6/469893353_559374650268333_1554092958913705032_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeGr4NSJHajZOoNz3pcIOrkl3030aItbiH7fTfRoi1uIfrU7oeHcfQ9lTLWagtsTiB0&_nc_ohc=uGdDMTVcAagQ7kNvgGCb05c&_nc_oc=AdjZ2tfA9lDR3M9oHgHx6puG2aAZZ6fvChZqsNZ-NuNVCoIoI6VZdGjiOlEPIFg437oNK9tTjvnAp6buvek10Bow&_nc_zt=23&_nc_ht=scontent.fbkk2-7.fna&_nc_gid=AnBnE8gyaQ11f2JwH_OAG75&oh=00_AYCSVbsKz0tAHZNa-OX7sjqmNzg3qR6PSEkfvcGa7mxesw&oe=67743D60',
+    registrationUrl: 'https://www.facebook.com/profile.php?id=100085874095905'
+  },
+  {
+    id: '4',
+    name: 'Dong Huai Luang Trail & Cross Country',
+    date: '2025-01-11',
+    location: 'เพชรบุรี',
+    distance: '30 km',
+    description: 'ดงห้วยหลวง ครอสคันทรี่ ',
+    imageUrl: 'https://scontent.fbkk2-8.fna.fbcdn.net/v/t39.30808-6/453221914_807050424750410_5850122967647747136_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeEP8OU8eiSk3u8ejPZ3Qi-78j9p-O3EHtTyP2n47cQe1OalDMIhgyGeuSdzB1QGJIA&_nc_ohc=0d4cLsUlbOkQ7kNvgHuNxNi&_nc_oc=AdgjTCEpbdGH0Ay3s1DgJ06qdYEMRj_Tj8GBFaWBIC20rCV9WEcrR2HR5Q8zUu4MNT00oL_JL-fEYtUCnhyHW_K_&_nc_zt=23&_nc_ht=scontent.fbkk2-8.fna&_nc_gid=A2pd-LbWUlU3oyLJrtU72EX&oh=00_AYCS_hSyaaNN_8pxtMW_sY4m3qoTRGur0YYp-wVhhSoX1Q&oe=677466DE',
+    registrationUrl: 'https://www.facebook.com/donghuailuangrun'
   }
 ];
